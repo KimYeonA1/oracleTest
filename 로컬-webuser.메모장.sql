8512614fb8ab0f31fa3f000326b5248d8d51c73c
KimYeonA1 <85yuna@naver.com>
2024-12-09 09:13:37 +0900

    회원가입 테이블 테스팅

--- 로컬-webuser.메모장.sql
+++ 로컬-webuser.메모장.sql
@@ -14,3 +14,37 @@ CREATE table "VISIT" (
 );
 ALTER TABLE VISIT ADD CONSTRAINT VISIT_NO_PK PRIMARY KEY(NO);
 INSERT INTO VISIT VALUES(VISIT_seq.NEXTVAL, 'KYA', '처음', SYSDATE);
+select * from visit order by no desc;
+
+--********************************************************************
+CREATE table LOGIN (
+    ID      VARCHAR2(12) NOT NULL,
+    PASS    VARCHAR2(12) NOT NULL
+);
+ALTER TABLE LOGIN ADD CONSTRAINT VISIT_ID_PK PRIMARY KEY(ID);
+  
+--********************************************************************
+
+create table ACCOUNT(
+    NO         NUMBER(5,0) NOT NULL,
+    NAME     VARCHAR2(20) NOT NULL,
+    ID     VARCHAR2(4000) NOT NULL,
+    PWD     VARCHAR2(4000) NOT NULL,
+    REGDATE   DATE NOT NULL
+);
+alter table ACCOUNT add constraint ACCOUNT_NO_PK primary key (NO);
+alter table ACCOUNT add constraint ACCOUNT_ID_UQ UNIQUE(ID);
+
+INSERT INTO ACCOUNT VALUES((select NVL(max(no),0)+1 from account),'AAA','AAA','AAA',SYSDATE);
+select * from account;
+
+SELECT PWD, NAME FROM ACCOUNT WHERE ID = '85yuna';
+
+commit;
+--===========================
+
+
+
+
+
+ 
