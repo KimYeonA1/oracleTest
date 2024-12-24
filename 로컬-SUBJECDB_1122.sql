2f2e9a79c2907918a16d199b1df67881a90d2738
KimYeonA1 <85yuna@naver.com>
2024-12-24 16:45:07 +0900

    병원정보 공공데이터 가져오기 병원테이블 생성

--- 로컬-SUBJECDB_1122.sql
+++ 로컬-SUBJECDB_1122.sql
@@ -1,5 +1,35 @@
+-- DROP 기존 테이블 및 시퀀스
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE SUBJECT CASCADE CONSTRAINTS';  --테이블
+    EXECUTE IMMEDIATE 'DROP TABLE STUDENT CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP TABLE TRAINEE CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP TABLE LESSON CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP SEQUENCE SUBJECT_SEQ';              --시퀀스
+    EXECUTE IMMEDIATE 'DROP SEQUENCE STUDENT_SEQ';
+    EXECUTE IMMEDIATE 'DROP SEQUENCE LESSON_SEQ';
+    EXECUTE IMMEDIATE 'DROP SEQUENCE TRAINEE_SEQ';
+EXCEPTION
+    WHEN OTHERS THEN NULL;
+END;
+/
+
 -- 학과 (01-컴퓨터학과 / 02-교육학과 / 03-신문방송학과 / 04-인터넷비즈니스과 / 05-기술경영과)
 create table subject( 
+    no number,                 -- pk, seq
+    num varchar2(2) not null,  -- 학과번호 01, 02, 03,04,05
+    name varchar2(24) not null -- 학과이름
+);
+Alter table subject add constraint subject_no_pk primary key(no); 
+Alter table subject add constraint subject_num_uk UNIQUE(num);
+
+create sequence subject_seq
+start with 1
+increment by 1; 
+
+-- 학생
+create table student( 
+    no number,                    --pk, seq
+    num varchar2(8) not null,     --학번(년도학과번호)
     name varchar2(12) not null,   --이름
     id varchar2(12) not null,     --아이디
     passwd varchar2(12) not null, --패스워드
@@ -7,16 +37,24 @@ create table subject(
     birthday varchar2(8) not null,--생년월일 
     phone varchar2(15) not null,  --전화번호
     address varchar2(80) not null,--주소
+    email varchar2(40) not null,  --이메일
+    sdate date default sysdate    --등록일
+);
+Alter table student add constraint student_no_pk primary key(no); 
+Alter table student add constraint student_id_uk UNIQUE(id);
+Alter table student add constraint student_num_uk UNIQUE(num);
+Alter table student add constraint student_subject_num_fk 
+    FOREIGN key(s_num) References subject(num) on delete set null;
+alter table student drop constraint student_subject_num_fk;
 
+create sequence student_seq
+start with 1
+increment by 1; 
 -- SUBJECT STUDENT INNER JOIN
 SELECT STU.NO, STU.NUM, STU.NAME, STU.ID,PASSWD,STU.S_NUM,SUB.NAME AS SUBJECT_NAME ,BIRTHDAY,PHONE,ADDRESS, EMAIL, SDATE
 FROM STUDENT STU INNER JOIN SUBJECT SUB ON STU.S_NUM = SUB.NUM ; 
---학생검색
-SELECT NUM, NAME, EMAIL FROM STUDENT WHERE NAME = 'KDJ';
-
 
 -- lesson 과목
-drop table lesson;
 create table lesson( 
     no number ,                 --pk seq
     abbre varchar2(2) not null, --과목요약
@@ -24,12 +62,12 @@ create table lesson(
 );
 Alter table lesson add constraint lesson_no_pk primary key(no); 
 Alter table lesson add constraint lesson_abbre_uk UNIQUE(abbre);
-@@ -77,21 +78,31 @@ 
+
+create sequence lesson_seq 
 start with 1
 increment by 1;
 
 -- trainee 수강신청
-drop table trainee;
 create table trainee( 
     no number ,                     --pk seq
     s_num varchar2(8),              --student.num(fk) 학생번호
@@ -40,33 +78,16 @@ create table trainee(
 select T.no, T.section, T.regdate, S.num, S.name, L.abbre, L.name as abbreName from trainee T inner join student S on T.s_num = S.num
                        inner join lesson L on T.abbre = L.abbre 
          order by T.no; 
+
 Alter table trainee add constraint trainee_no_pk primary key(no);
 Alter table trainee add constraint trainee_student_num_fk 
     FOREIGN key(s_num) References student(num) on delete set null;
 Alter table trainee add constraint trainee_lesson_abbre_fk 
     FOREIGN key(abbre) References lesson(abbre) on delete set null;
 Alter table trainee drop constraint trainee_lesson_abbre_fk; 
-
+    
 create sequence trainee_seq 
 start with 1
 increment by 1;
 
--- 테스팅
-UPDATE TRAINEE SET S_NUM = '', ABBRE = '', SECTION = '' WHERE NO = 10; 
-INSERT INTO TRAINEE VALUES(trainee_seq.NEXTVAL,'','','',SYSDATE); 
-
---공공데이터 (전통시장 정보)
-create table LANDPRICE(
-    NODENO NUMBER,
-    GPSLATI NUMBER,
-    GPSLONG NUMBER,
-    NODEID VARCHAR2(20) NOT NULL,
-    NODENM VARCHAR2(50)
-);
-Alter table LANDPRICE add constraint LANDPRICE_NODENO_PK primary key(NODENO); 
-Alter table LANDPRICE add constraint LANDPRICE_NODEID_UK UNIQUE(NODEID);
-
-SELECT count *
-
-
-
+COMMIT;
